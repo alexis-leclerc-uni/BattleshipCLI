7b3541ba8a8e9933fe2fe905ddad6e36d3d3c238
Douglas-Mcmuffin <gacote18@gmail.com>
2024-02-22 12:05:42 -0500

    modif

---
+++ ConsoleApplication3/lib/jeu.cpp
@@ -0,0 +1,163 @@
+#include "jeu.h"
+
+//Description : Construit le jeu
+//Entrée : rien
+//Sortie : rien
+Jeu::Jeu()
+{
+}
+//Description : Déconstruit le jeu (déallouer la mémoire des Joueur)
+//Entrée : rien
+//Sortie : rien
+Jeu::~Jeu()
+{
+    for (int i = 0; i < vecJoueur.size(); i++)
+        delete vecJoueur[i];
+}
+//Description : Afficher le menu de démarrage (Demande s'il veut commencer et le joueur doit répondre par y/n)
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherStartUp(std::ostream& sout)
+{
+    return false;
+}
+//Description : Demande au joueur s'il veut commencer une partie ou quitter
+//Entrée : un canal de communication
+//Sortie : 0 pour Confirmer et 1 pour Quitter
+int Jeu::menuStartUp(std::ostream& sout, std::istream& sin)
+{
+    afficherStartUp(sout);
+    char c;
+    sin >> c;
+    if (c == 'y' || c == 'Y')
+        return CONFIRMER;
+    if (c == 'n' || c == 'n')
+        return QUITTER;
+    return INCORRECT;
+}
+//Description : Afficher le menu de réglage
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherReglage(std::ostream& sout)
+{
+    return false;
+}
+//Description : Afficher le menu de réglage (demande la taille en X)
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherTailleEnX(std::ostream& sout)
+{
+    return false;
+}
+//Description : Afficher le menu de réglage (demande la taille en Y)
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherTailleEnY(std::ostream& sout)
+{
+    return false;
+}
+//Description : Demande au joueur la taille de la carte
+//Entrée : un canal de communication
+//Sortie : 0 pour Confirmer la taille choisie, 1 pour revenir en arrière
+int Jeu::menuReglage(std::ostream& sout, std::istream& sin)
+{
+    afficherReglage(sout);
+    do {
+        afficherTailleEnX(sout);
+        sin >> tailleEnX;
+    } while (tailleEnX <= 0);
+    do {
+        afficherTailleEnY(sout);
+        sin >> tailleEnY;
+    } while (tailleEnY <= 0);
+
+    return CONFIRMER;
+}
+//Description : Afficher le menu pour que le joueur initialise ses bateaux
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherInitJoueur(std::ostream& sout, Joueur joueur)
+{
+    return false;
+}
+//Description : Indique la taille du bateau
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherInitTaille(std::ostream& sout, Joueur joueur, int taille)
+{
+    return false;
+}
+//Description : Le joueur installe ses bateaux
+//Entrée : un canal de communication et la taille du bateau
+//Sortie : 0 pour Confirmer, 1 pour revenir en arrière
+int Jeu::menuInitJoueur(std::ostream& sout, std::istream& sin,Joueur* joueur)
+{
+    afficherInitJoueur(sout, *joueur);
+    int tailleBateau[] = {5,4,3,3,2};
+    int x = -1; int y = -1;
+    bool horizontal;
+    for (int i = 0; i < 5; i++)
+    {
+        do {
+            afficherInitTaille(sout, *joueur, tailleBateau[i]);
+            sin >> x >> y >> horizontal;
+        } while (joueur->ajouterBateau(x,y,horizontal,tailleBateau[i]));
+        
+    }
+    return CONFIRMER;
+}
+//Description : Afficher le menu pour que le joueur tir sur l'adversaire
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherTir(std::ostream& sout, Joueur joueur, Joueur adversaire)
+{
+    return false;
+}
+//Description : Le joueur tir sur son adversaire
+//Entrée : un canal de communication
+//Sortie : 0 si tout va bien
+int Jeu::menuTir(std::ostream& sout, std::istream& sin,Joueur* joueur, Joueur* adversaire)
+{
+    Coordonnee cord = {-1,-1};
+    do {
+        afficherTir(sout, *joueur, *adversaire);
+        sin >> cord.x >> cord.y;
+    } while (!joueur->tirer(cord, adversaire));
+    return false;
+}
+//Description : Afficher le menu de fin
+//Entrée : un canal de communication
+//Sortie : Vrai si ça affiche
+bool Jeu::afficherFin(std::ostream& sout)
+{
+    return false;
+}
+//Description : Demande au joueur s'il veut continuer
+//Entrée : un canal de communication
+//Sortie : 0 si on recommence, 1 si on quitte
+int Jeu::menuFin(std::ostream& sout, std::istream& sin)
+{
+    afficherFin(sout);
+    char c;
+    sin >> c;
+    if (c == 'y' || c == 'Y')
+        return CONFIRMER;
+    if (c == 'n' || c == 'n')
+        return QUITTER;
+    return INCORRECT;
+}
+//Description : Ajoute un joueur au vector
+//Entrée : rien
+//Sortie : rien
+void Jeu::ajouterJoueur()
+{
+    vecJoueur.push_back(new Joueur(tailleEnX,tailleEnY));
+    return;
+}
+//Description : renvoie le joueur sélectionné
+//Entrée : rien
+//Sortie : le pointeur du joueur sélectionné
+Joueur* Jeu::getJoueur(int index)
+{
+    return vecJoueur[index];
+}
---
+++ ConsoleApplication3/lib/jeu.h
@@ -0,0 +1,36 @@
+#ifndef JEU
+#define JEU
+
+#define CONFIRMER 0
+#define QUITTER 1
+#define INCORRECT 2
+
+#include <iostream>
+#include "Joueur.h"
+
+class Jeu {
+    public:
+        Jeu();
+        ~Jeu();
+        bool afficherStartUp(std::ostream& sout);
+        int menuStartUp(std::ostream& sout, std::istream& sin);
+        bool afficherReglage(std::ostream& sout);
+        bool afficherTailleEnX(std::ostream& sout);
+        bool afficherTailleEnY(std::ostream& sout);
+        int menuReglage(std::ostream& sout, std::istream& sin);
+        bool afficherInitJoueur(std::ostream& sout, Joueur joueur);
+        bool afficherInitTaille(std::ostream& sout, Joueur joueur, int taille);
+        int menuInitJoueur(std::ostream& sout, std::istream& sin,Joueur* joueur);
+        bool afficherTir(std::ostream& sout, Joueur joueur, Joueur adversaire);
+        int menuTir(std::ostream& sout, std::istream& sin,Joueur* joueur, Joueur* adversaire);
+        bool afficherFin(std::ostream& sout);
+        int menuFin(std::ostream& sout, std::istream& sin);
+        void ajouterJoueur();
+        Joueur* getJoueur(int index);
+    private:
+        int tailleEnX;
+        int tailleEnY;
+        std::vector<Joueur*> vecJoueur;
+};
+
+#endif
